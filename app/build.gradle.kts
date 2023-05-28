592201638ccea0954f3aeede03cbb07886eb88ca
dsh <dsh@dman13.dyndns.org>
2023-05-27 21:44:33 -0400

    Enable Jetpack Compose

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -230,6 +230,10 @@ android {
             jvmTarget = jvmTargetVersion
         }
     }
+    composeOptions {
+        // https://developer.android.com/jetpack/androidx/releases/compose-kotlin
+        kotlinCompilerExtensionVersion = "1.4.6"
+    }
 
     testOptions {
         // prevent logger errors
@@ -266,9 +270,9 @@ android {
     }
 
     buildFeatures {
+        compose = true
         viewBinding = true
     }
-
     namespace = "net.bible.android.activity"
 }
 
@@ -318,6 +322,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.core:core-ktx:1.10.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
     implementation("androidx.preference:preference:1.2.0")
     implementation("androidx.preference:preference-ktx:1.2.0")
     implementation("androidx.recyclerview:recyclerview:1.3.0")
@@ -326,6 +331,22 @@ dependencies {
     implementation("net.objecthunter:exp4j:0.4.8")
 
     //implementation("androidx.recyclerview:recyclerview-selection:1.0.0")
+
+    val composeBom = platform("androidx.compose:compose-bom:2023.05.01")
+    implementation(composeBom)
+    androidTestImplementation(composeBom)
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-viewbinding")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation("com.google.accompanist:accompanist-themeadapter-appcompat:0.30.1")
+    // Android Studio Preview support
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    // Tests of Compose-based UI
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
 
     //implementation("com.jaredrummler:colorpicker:1.1.0")
     implementation("com.github.AndBible:ColorPicker:ab-fix-1")
